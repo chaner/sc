8705897944fd7e06d19c1e4180469e82ee7c0b97
Eric Chan <chantastic@Erics-Mac-mini.local>
2015-08-29 11:19:25 -0700

    add test for caps

--- spec/caps_spec.rb
+++ spec/caps_spec.rb
@@ -6,4 +6,10 @@ describe 'caps' do
     expect(caps('SocialChorus.com')).to eq('soCiaLchOruS.coM')
     expect(caps('shouldWeIgnoreSpaces?')).to eq('shOulDweIgnOreSpaCes?')
   end
+
+  it 'does not change the original string' do
+    original = 'SocialChorus.com'
+    expect(caps(original)).to eq('soCiaLchOruS.coM')
+    expect(original).to eq('SocialChorus.com')
+  end
 end
